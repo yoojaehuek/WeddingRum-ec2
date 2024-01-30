73fa9ebe21908bce621cfc6b71fed33c74788b71
yoojh <jaehek2629@naver.com>
2024-01-30 16:31:28 +0900

    yjh

--- wedingrum/src/config/contansts.js
+++ wedingrum/src/config/contansts.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://www.kimjunnyoung.com/app3";
+export const API_URL = "http://43.202.248.142/app3";
 // export const API_URL = "https://lamp-shopping-server-7ingout.herokuapp.com";
